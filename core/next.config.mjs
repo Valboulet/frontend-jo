18d06f86941412161e34e20b64f4480d94173ba4
Valboulet <valboulet59@gmail.com>
2024-10-21 11:52:18 +0200

    Updated next.config

--- core/next.config.mjs
+++ core/next.config.mjs
@@ -3,9 +3,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: process.env.NODE_ENV === "production" ? "https" : "http",
-        hostname: process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_API_HOST.replace('https://', '').replace('http://', '') : "localhost",
-        port: process.env.NODE_ENV === "production" ? "" : "8000",
+        protocol:'https',
+        hostname: 'backjo2024.up.railway.app',
+        port: '',
         pathname: "/**",
       },
     ],
