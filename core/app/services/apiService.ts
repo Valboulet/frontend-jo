804de1b5aee5031ec198ad3b505e415e4f3ea19d
Valboulet <valboulet59@gmail.com>
2024-10-21 14:33:20 +0200

    test apiService to deploy

--- core/app/services/apiService.ts
+++ core/app/services/apiService.ts
@@ -24,34 +24,34 @@ const apiService = {
         })
     },
 
-    // post: async function(url: string, data: any): Promise<any> {
-    //     console.log('post', url, data);
+    post: async function(url: string, data: any): Promise<any> {
+        console.log('post', url, data);
 
-    //     const token = await getAccessToken();
+        const token = await getAccessToken();
 
-    //     return new Promise((resolve, reject) => {
-    //         fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
-    //             method: 'POST',
-    //             body: data,
-    //             headers: {
-    //                 'Accept': 'application/json',
-    //                 'Content-Type': 'application/json'
+        return new Promise((resolve, reject) => {
+            fetch(`https://backjo2024.up.railway.app${url}`, {
+                method: 'POST',
+                body: data,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
 
-    //             // headers: {
-    //             //     'Authorization': `Bearer ${token}`
-    //             }
-    //         })
-    //             .then(response => response.json())
-    //             .then((json) => {
-    //                 console.log('Response:', json);
+                // headers: {
+                //     'Authorization': `Bearer ${token}`
+                }
+            })
+                .then(response => response.json())
+                .then((json) => {
+                    console.log('Response:', json);
 
-    //                 resolve(json);
-    //             })
-    //             .catch((error => {
-    //                 reject(error);
-    //             }))
-    //     })
-    // },
+                    resolve(json);
+                })
+                .catch((error => {
+                    reject(error);
+                }))
+        })
+    },
 }
 
 export default apiService;
